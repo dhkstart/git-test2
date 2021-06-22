a70e23790b0cb99b7bec0f1447a6affbf9a48037
dhkstart <hongkangding@163.com>
2021-06-22 15:55:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author dhkstart  邮箱：hongkangding@163.com
+ * @create 2021-06-22-15:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello,git!");
+    }
+
+}
